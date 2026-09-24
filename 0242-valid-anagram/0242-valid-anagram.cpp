b55d82511f8d2c1ab45d4fdcc07bebdd3054ca07
Sargam Singh <thisissargam@gmail.com>
2026-09-24 14:13:09 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,14 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
         return false;
-        unordered_map<char,int>countS;
-        unordered_map<char,int>countT;
-        for(int i=0;i<s.length();i++){
-            countS[s[i]]++;
-            countT[t[i]]++;
+        int f[26]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            f[s[i]-'a']++;
+            f[t[i]-'a']--;
         }
-        if(countT==countS)
+        for(int i=0;i<26;i++)
+        {
+            if(f[i]!=0) return false;
+        }
         return true;
-        return false;
     }
 };
